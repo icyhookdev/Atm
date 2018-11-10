e3c9e1cc78d37bc97dba16758a29b277ef1b7338
thienJs <cristian.albarran95@gmail.com>
2018-11-10 15:06:48 -0400

    keyboard design v1.0.0

--- src/app/atm/atm.component.ts
+++ src/app/atm/atm.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import Typed from 'typed.js';
+
+
+
 @Component({
   selector: 'app-atm',
   templateUrl: './atm.component.html',
   styleUrls: ['./atm.component.css']
 })
 export class AtmComponent implements OnInit {
-
+  showScreen = false;
   constructor() { }
 
   ngOnInit() {
+    const typed = new Typed('.element', {
+      strings: ['Mas Que Un Banco.', 'Agentes Efectivos en el Area.', 'Inter Bank Donde Todo Es Posible.'],
+      smartBackspace: true, // Default value
+      backDelay: 2000,
+      typeSpeed: 70,
+      backSpeed: 70,
+      loop: true,
+      loopCount: Infinity,
+    });
   }
 
 }
