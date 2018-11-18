2c8f3810511d0570c0a88d5a9fa245d336980111
thienJs <cristian.albarran95@gmail.com>
2018-11-17 20:43:44 -0400

    menu component ui created

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import Typed from 'typed.js';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -10,6 +11,17 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const typed = new Typed('.welcome', {
+      strings: ['Welcome', 'Bienvenido', 'Bienvenue'],
+      smartBackspace: true, // Default value
+      backDelay: 2000,
+      typeSpeed: 70,
+      backSpeed: 70,
+      loop: true,
+      loopCount: Infinity,
+      showCursor: true,
+      cursorChar: '.',
+    });
   }
 
 }
